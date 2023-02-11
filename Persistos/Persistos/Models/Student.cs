960f7f57c78bb6979e89dc6fc7a5584baaccaeb7
SeymenNW <seymennw@gmail.com>
2023-02-11 12:43:47 +0100

    Created basic databinding. Updated UI.

---
+++ Persistos/Persistos/Models/Student.cs
@@ -0,0 +1,42 @@
+﻿using Avalonia;
+using Avalonia.Animation;
+using Avalonia.Controls;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistos.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Major { get; set; }
+        public int Semester { get; set; }
+
+        public Student() 
+        { 
+
+        }
+
+       public Student(string name, string major, int semester)
+        {
+            Name = name;
+            Major = major;
+            Semester = semester;
+        }
+
+
+        public override string ToString()
+        {
+            return $"Name: {Name} | Major: {Major} | Semester: {Semester}";
+        }
+
+
+
+    }
+}
--- Persistos/Persistos/ViewModels/MainWindowViewModel.cs
+++ Persistos/Persistos/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,34 @@
-﻿namespace Persistos.ViewModels;
+﻿using Persistos.Models;
+using System.Collections.ObjectModel;
+
+namespace Persistos.ViewModels;
+
 
 public class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting => "Welcome to Avalonia!";
+   public Student student = new Student();
+
+    public ObservableCollection<Student> studentsList { get; set; }
+
+    public MainWindowViewModel() 
+    {
+
+        studentsList = new ObservableCollection<Student>()
+    {
+        new Student() { Major="Biology", Name="Jake", Semester=5},
+        new Student() { Major="Mathematics", Name="Oliver", Semester=2},
+         new Student() { Major="Biology", Name="Jake", Semester=5},
+         new Student() { Major="Biology", Name="Jake", Semester=5},
+        new Student() { Major="Mathematics", Name="Oliver", Semester=2},
+         new Student() { Major="Biology", Name="Jake", Semester=5},
+        new Student() { Major="Mathematics", Name="Oliver", Semester=2}
+
+
+
+    };
+
+    }
+
+
+
 }
